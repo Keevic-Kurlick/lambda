244ea2cb7c3f95109f6d2a6b097661100d384981
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-07-11 20:21:32 +0300

    solution with 33 wrong task

--- src/ru/netology/Worker.java
+++ src/ru/netology/Worker.java
@@ -1,14 +1,24 @@
 package ru.netology;
 
-public class Worker extends Main {
+public class Worker {
     private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
 
     public Worker(OnTaskDoneListener callback) {
         this.callback = callback;
     }
 
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
     public void start() {
         for (int i = 0; i < 100; i++) {
+            if (i==33) {
+                errorCallback.onError("Task " + i + " is wrong");
+                i++;
+            }
             callback.onDone("Task " + i + " is done");
         }
     }
